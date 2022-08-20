a49481d34a309beef2850079b42d4964c17648d2
김기윤 <crakel528@ajou.ac.kr>
2022-08-20 19:52:55 +0900

    add MemberServiceTest

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,12 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // constructor로 객체 생성 (new로 직접 생성 x)
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
